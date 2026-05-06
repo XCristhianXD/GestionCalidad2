3248c3a985d58b8c36657f9ef7185c9ad6b3b48c
XCristhianXD <cristhianamador777@gmail.com>
2026-05-05 23:29:31 -0400

    arreglado creooooo

--- Program.cs
+++ Program.cs
@@ -4,12 +4,20 @@ using GestionCalidad.Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("GestionCalidadContext")
+// 🔥 Puerto dinámico (Render / Docker)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+// 🔥 Connection string (local + Render)
+var connectionString =
+    builder.Configuration.GetConnectionString("GestionCalidadContext")
     ?? Environment.GetEnvironmentVariable("ConnectionStrings__GestionCalidadContext");
 
+// DbContext
 builder.Services.AddDbContext<GestionCalidadContext>(options =>
     options.UseNpgsql(connectionString));
 
+// Services
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -17,21 +25,23 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configuración del puerto igual a tu código
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
-// Migraciones directas sin try-catch (como el tuyo)
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<GestionCalidadContext>();
-    context.Database.Migrate(); 
+    context.Database.Migrate();
 }
+
+// Swagger solo en desarrollo
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+
 app.UseAuthorization();
 app.MapControllers();
+
+// 🔥 Migraciones automáticas (base de datos en cloud)
+//Hola
+
 
 app.Run();
